bc1e1e1b3cc25892ed24bdf82e090b74a75f784b
Jonathan <jonathancjonsson@gmail.com>
2024-11-25 18:27:58 +0100

    Add game manager and increase health on health pickup

--- scripts/heart.gd
+++ scripts/heart.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+@onready var player_data_manager: Node = %PlayerDataManager
 
 func _on_body_entered(body: Node2D) -> void:
+	player_data_manager.increase_health()
 	queue_free()
---
+++ scripts/player_data_manager.gd
@@ -0,0 +1,7 @@
+extends Node
+
+var health = 0
+
+func increase_health():
+	health += 1
+	print(health)
